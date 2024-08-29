202388b7c3451a731d680a75374bf87b07c2125d
risingsaya12345 <m.benattra22035@pi.tngit config --global user.name risingsaya12345git config --global user.email m.benattra22035@pi.tn>
2024-08-29 12:49:38 +0100

    the error that i made on pushing myy project on the wrong branch was reverted!!,thus i used i used git revert HEAD , then git push origin main (to restore the previouus push from main)

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
-import React, { useState } from 'react';
-import Sidebar from './components/Sidebar';
-import VerticalNavbar from './components/VerticalNavbar';
+import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [activePage, setActivePage] = useState('');
-
-  const handleIconClick = (page) => {
-    setActivePage(page); // Set active page based on the clicked icon
-  };
-
   return (
-    <div className="app">
-      <Sidebar onIconClick={handleIconClick} />
-      {activePage && <VerticalNavbar activePage={activePage} />}
-      <div className="content-container">
-        {activePage && (
-          <div className={`page-window ${activePage}`}>
-            <div className="page-content">
-              <h1>{activePage} Page</h1>
-              <p>This is the {activePage} section content.</p>
-            </div>
-          </div>
-        )}
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
