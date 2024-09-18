6f7a7da3b4747b2828abdf03307ce804a6507a03
risingsaya12345 <m.benattra22035@pi.tngit config --global user.name risingsaya12345git config --global user.email m.benattra22035@pi.tn>
2024-09-18 20:53:22 +0100

    Updated Firebase config with new account details

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,16 +4,17 @@ import { getFirestore } from 'firebase/firestore'; // Import Firestore
 import { getStorage } from 'firebase/storage'; // Import Firebase Storage
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDqRGhdSrDVhK1685_HPhJwrB-65Hmd9bs",
-    authDomain: "skillmate-29f38.firebaseapp.com",
-    projectId: "skillmate-29f38",
-    storageBucket: "skillmate-29f38.appspot.com",
-    messagingSenderId: "74279534766",
-    appId: "1:74279534766:web:e27561f2216e9e7bce2d3f",
-    measurementId: "G-RVPWBVY027"
+    apiKey: "AIzaSyBMjUSnWZKfn53Zp4PohhXdU8Z34zY2pqo",
+    authDomain: "skillmate-8a826.firebaseapp.com",
+    projectId: "skillmate-8a826",
+    storageBucket: "skillmate-8a826.appspot.com",
+    messagingSenderId: "497215767576",
+    appId: "1:497215767576:web:0c3e4a5aef02792b66ef6d",
+    measurementId: "G-VT1M5GH42C"
 };
 
 const app = initializeApp(firebaseConfig);
+
 
 // Initialize Firestore and Storage
 const db = getFirestore(app); // Initialize Firestore
